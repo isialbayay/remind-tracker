c64bc8077e4c3741b03f5a845ef42dc33d3f7205
Esteban Esparza <e.esparza02@ufromail.cl>
2022-06-27 22:15:39 -0400

    Se refactoriza store user

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/userStore.js'
 import 'bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
